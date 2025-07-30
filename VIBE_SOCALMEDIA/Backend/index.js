70717e0b720bf684cc52c9c78e31366a65459d1f
irfanbaig7 <ibaig9356@gmail.com>
2025-07-30 21:44:01 +0530

    create project basic folder structure, insatlling some packages /dependencies, and Make Server Successfully..

--- VIBE_SOCALMEDIA/Backend/index.js
+++ VIBE_SOCALMEDIA/Backend/index.js
@@ -1,11 +1,11 @@
 import express from "express"
 const app = express()
+const port = process.env.PORT || 4000
 
 app.get("/", (req, res) => {
     res.send("Ready for Social Media Project")
 })
 
-const port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log("Server starded Successfully");
