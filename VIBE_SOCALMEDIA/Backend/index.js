e4d798916244f996ed39d260625317fda41e83e1
irfanbaig7 <ibaig9356@gmail.com>
2025-08-01 00:24:26 +0530

    configure here authRouter and add some middlewares (cookieparser, cors) and express.json()

--- VIBE_SOCALMEDIA/Backend/index.js
+++ VIBE_SOCALMEDIA/Backend/index.js
@@ -1,7 +1,21 @@
 import express from "express"
 import connectDb from "./config/dbConection.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 const app = express()
 const port = process.env.PORT || 4000
+
+
+// middele Ware
+app.use(cors({
+    origin: "http://localhost:5173",
+    withCredentials: true
+}))
+app.use(express.json()) // JSON format mein bheje gaye data ko read karne ke liye (req.body me store hota hai)
+app.use(cookieParser())
+app.use("api/auth", authRouter)
+
 
 app.listen(port, () => {
     connectDb()
