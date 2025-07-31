d9833998ce9ca9de4d9a5515e1b252ab541a5f31
irfanbaig7 <ibaig9356@gmail.com>
2025-08-01 00:22:59 +0530

    create routes (signup, signin) for controllers

---
+++ VIBE_SOCALMEDIA/Backend/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express from "express"
+import {signUp, signIn} from "../controller/auth.controller.js"
+const authRouter = express.Router()
+
+authRouter.post("/signup", signUp)
+authRouter.post("/signin", signIn)
+
+export default authRouter
