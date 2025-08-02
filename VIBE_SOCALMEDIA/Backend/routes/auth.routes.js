2d2d0c8b3275a88ec7b85bfbef8e27fc6d40f377
irfanbaig7 <ibaig9356@gmail.com>
2025-08-02 17:00:37 +0530

    create JWT token and also Auth Controller with signup, signin, and singout

--- VIBE_SOCALMEDIA/Backend/routes/auth.routes.js
+++ VIBE_SOCALMEDIA/Backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {signUp, signIn} from "../controller/auth.controller.js"
+import {signUp, signIn, signOut} from "../controller/auth.controller.js"
 const authRouter = express.Router()
 
 authRouter.post("/signup", signUp)
 authRouter.post("/signin", signIn)
+authRouter.post("/signin", signOut)
 
 export default authRouter
